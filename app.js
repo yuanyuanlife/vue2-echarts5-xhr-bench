82c43c566a099387842af04764a06ec567aa622c
Yuan Yuan <2127129+yuanyuanlife@users.noreply.github.com>
2022-02-22 17:05:53 +0800

    Create static express server

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -15,7 +14,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Vue2 ECharts5 XHR Bench" });
+  res.render("index");
 });
 
 module.exports = router;
